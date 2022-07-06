9d756bb454bdf04d5bf95df2e14a351cea2661aa
McleanTheNerd <mcleanmartinchikadaya@gmail.com>
2022-07-06 11:45:44 +0100

    procube

--- procube/urls.py
+++ procube/urls.py
@@ -6,5 +6,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('contrib.auth.urls'))
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
