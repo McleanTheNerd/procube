fa44dde3f6afe67a0e6f31c2626810ae2953b5cf
McleanTheNerd <mcleanmartinchikadaya@gmail.com>
2022-07-06 20:52:50 +0100

    db

--- core/views/authentication.py
+++ core/views/authentication.py
@@ -4,7 +4,4 @@ def login(request):
     return render(request,'registration/login.html')
 
 def login_process(request):
-    if request.method =! 'POST':
-        pass
-    else:
-        pass
+    pass
